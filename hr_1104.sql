3320d767b47ee1064520521de49102bb3de46849
kdongjin <rlaeogus0911@nate.com>
2024-11-06 11:31:43 +0900

    nvl, nvl2 , to_char, to_number, to_date, sysdate 테스팅

--- hr_1104.sql
+++ hr_1104.sql
@@ -53,6 +53,44 @@ SELECT DEPARTMENT_ID, SALARY FROM EMPLOYEES WHERE DEPARTMENT_ID >= 70;
 SELECT DEPARTMENT_ID, SALARY FROM EMPLOYEES WHERE DEPARTMENT_ID >= 70 GROUP BY DEPARTMENT_ID ;
 SELECT DEPARTMENT_ID, MAX(SALARY), MIN(SALARY),SUM(SALARY) ROUND(AVG(SALARY),1), COUNT(SALARY) FROM EMPLOYEES WHERE DEPARTMENT_ID >= 70 GROUP BY DEPARTMENT_ID ;
 
+-- 03년도에 입사한 사원 알아내기
+Select 입사년도, 사원이름 from employees where TO_CHAR(hire_date, ‘YY’) = ‘03’;  
+select hire_date, TO_CHAR(hire_date,'YY/MM/DD HH24:MI:SS'),  TO_CHAR(hire_date,'MI')  from employees;
+select TO_DATE('20041214','YYYY/MM/DD')+1 from dual; 
+
+-- 이름이 k로 끝나는 직원을 검색
+select first_name from employees;
+select first_name from employees where first_name like '%k';
+select first_name from employees where upper(substr(first_name, LENGTH(first_name),1)) = UPPER('k');
+
+-- 현재시간표현
+select sysdate from dual;
+select to_char(sysdate,'YYYY/MM/DD HH24:MI:SS') from dual;
+select FLOOR(sysdate - TO_DATE('2024/11/05', 'YYYY/MM/DD')) from dual;
+
+-- 숫자를 우리가 원하는 형식으로 출력하기 1234567.23 => $1,234,567.23
+select trim(to_char(1234567.23, 'L999,999,999,999.99')) as money from dual; 
+select first_name, trim(to_char(salary, 'L999,999,999,999.99')) as salary from employees; 
+
+-- 문자 + 문자 = 숫자
+select to_number('10,000', '999,999') + to_number('20,000','999,999') from dual;
+select to_number('10,000', '999999') from dual;
+
+-- NVL
+select first_name, salary, commission_pct, job_id from employees order by job_id;
+select first_name, salary, nvl(commission_pct,0) commission_pct, job_id from employees order by job_id;
+
+-- NVL2(대상, 널이아닐때 적용될값, 널일때 적용될값)
+select first_name, salary, commission_pct, salary+(salary*commission_pct) as total from employees; 
+select first_name, salary, commission_pct, salary+(salary*NVL(commission_pct,0)) as total from employees;
+select first_name, salary, commission_pct, salary+(salary*NVL2(commission_pct,commission_pct, 0)) as total from employees;
+
+
+
+
+
+
+
 
 
 
