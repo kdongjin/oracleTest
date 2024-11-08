7735f4ccec5c3468c9e19b77a403517ee31294e6
kdongjin <rlaeogus0911@nate.com>
2024-11-08 12:12:48 +0900

    시스템에서 사용자 계정 처리 방법, 권한설정 테스팅

--- SYSTEM_사용자생성.sql
+++ SYSTEM_사용자생성.sql
@@ -1,6 +1,7 @@
+-- 사용자 계정만들기 (시스템관리자모드에서 진행해야됨)
 ALTER SESSION SET "_ORACLE_SCRIPT"=true;
-DROP USER model2 CASCADE; -- 기존 사용자 삭제
-CREATE USER model2 IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
-    DEFAULT TABLESPACE USERS
-    TEMPORARY TABLESPACE TEMP;
-GRANT connect, resource, dba TO model2; -- 권한 부여
+DROP USER STUDENTDB CASCADE; -- 기존 사용자 삭제
+CREATE USER STUDENTDB IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
+    DEFAULT TABLESPACE USERS    -- 데이터 저장소
+    TEMPORARY TABLESPACE TEMP;   -- 임시저장장소
+GRANT connect, resource, dba TO STUDENTDB; -- 권한 부여
