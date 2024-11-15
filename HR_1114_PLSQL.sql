31c80dcc30cdfca8c5fa5449fc1e43b27d5a7bbc
kdongjin <rlaeogus0911@nate.com>
2024-11-15 09:44:23 +0900

    PL/SQL Clara 사원의 (직무, 급여, 입사일자, 커미션)(employees), 부서명(departments)을 변수에 대입하여 출력 테스팅

--- HR_1114_PLSQL.sql
+++ HR_1114_PLSQL.sql
@@ -1,21 +1,32 @@
 -- PL/SQL 
--- EMPLOYEES 테이블에 등록된 총사원의 수와
--- 급여의 합, 급여의 평균을 변수에 대입하여 출력하 
-DECLARE
-    TOTAL_EMPLOYEE NUMBER(4);
-    TOTAL_SALARY NUMBER(10);
-    AVG_SALARY NUMBER(10);
-BEGIN
-   SELECT COUNT(EMPLOYEE_ID),SUM(SALARY),ROUND(AVG(SALARY))
-   INTO TOTAL_EMPLOYEE,TOTAL_SALARY,AVG_SALARY
-   FROM EMPLOYEES;
+-- Clara 사원의 (직무, 급여, 입사일자, 커미션)(employees), 부서명(departments)을 변수에 대입하여 출력
 
-   DBMS_OUTPUT.PUT_LINE('총사원의 수: '||TOTAL_EMPLOYEE);
-   DBMS_OUTPUT.PUT_LINE('급여의 합: '||TOTAL_SALARY);
-   DBMS_OUTPUT.PUT_LINE('급여의 평균: '||AVG_SALARY);
+select job_id, salary, hire_date, commission_pct, depratment_id from employees where first_name = 'Clara';
+select department_name, department_id from departments; 
+
+select job_id, salary, hire_date, commission_pct,department_name 
+from employees E inner join departments D on E.department_id = D.department_id where first_name = 'Clara';
+
+DECLARE
+    VFIRST_NAME EMPLOYEES.FIRST_NAME%TYPE;
+    VJOB_ID EMPLOYEES.JOB_ID%TYPE;
+    VSALARY EMPLOYEES.SALARY%TYPE;
+    VHIRE_DATE EMPLOYEES.HIRE_DATE%TYPE;
+    VCOMMISSION_PCT EMPLOYEES.COMMISSION_PCT%TYPE;
+    VDEPARTMENT_NAME DEPARTMENTS.DEPARTMENT_NAME%TYPE;
+BEGIN
+    SELECT FIRST_NAME, JOB_ID, SALARY, HIRE_DATE, COMMISSION_PCT, DEPARTMENT_NAME
+    INTO VFIRST_NAME, VJOB_ID, VSALARY, VHIRE_DATE, VCOMMISSION_PCT, VDEPARTMENT_NAME
+    FROM EMPLOYEES E INNER JOIN DEPARTMENTS D ON e.department_id=d.department_id
+    WHERE FIRST_NAME = 'Clara';
+
+    DBMS_OUTPUT.PUT_LINE('이름 : '||VFIRST_NAME);
+    DBMS_OUTPUT.PUT_LINE('직무 : '||VJOB_ID);
+    DBMS_OUTPUT.PUT_LINE('급여 : '||VSALARY);
+    DBMS_OUTPUT.PUT_LINE('입사일자 : '||VHIRE_DATE);
+    DBMS_OUTPUT.PUT_LINE('커미션 : '||VCOMMISSION_PCT);
+    DBMS_OUTPUT.PUT_LINE('부서명 : '||VDEPARTMENT_NAME);
 END;
 /
-
-select count(*), sum(salary), round(avg(salary)) from employees; 
 
 
