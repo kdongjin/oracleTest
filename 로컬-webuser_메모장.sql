54934f97573935371ca2ff9ae8d5699a869bea33
kdongjin <rlaeogus0911@nate.com>
2024-12-09 09:13:30 +0900

    김민석 회원가입 테이블 테스팅

--- 로컬-webuser_메모장.sql
+++ 로컬-webuser_메모장.sql
@@ -15,3 +15,24 @@ ALTER TABLE VISIT ADD CONSTRAINT VISIT_NO_PK PRIMARY KEY(NO);
 INSERT INTO VISIT VALUES(VISIT_seq.NEXTVAL, 'KDJ', '처음으로 메모장입력', SYSDATE);
 SELECT * FROM VISIT;
 select * from visit order by no desc;
+
+-- 김민석 회원가입 테이블
+create table ACCOUNT(
+    NO      NUMBER(5,0) NOT NULL,
+    NAME    VARCHAR2(20) NOT NULL,
+    ID      VARCHAR2(4000) NOT NULL,
+    PWD     VARCHAR2(4000) NOT NULL,
+    REGDATE DATE NOT NULL
+);
+alter table ACCOUNT add constraint ACCOUNT_NO_PK primary key (NO);
+alter table ACCOUNT add constraint ACCOUNT_ID_UQ UNIQUE(ID);
+
+INSERT INTO ACCOUNT VALUES((select NVL(max(no),0)+1 from ACCOUNT),'AAA','AAA','AAA',SYSDATE);
+SELECT PWD, NAME FROM ACCOUNT WHERE ID = 'kdj';
+SELECT * FROM ACCOUNT;
+commit;
+
+
+
+
+
